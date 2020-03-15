0147312ac66d17582b46048ff08781a308a2abbf
MacaEsru <163170@ids.upchiapas.edu.mx>
2020-03-15 16:51:26 -0600

    add appLogin

--- Login/views.py
+++ Login/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics
+
+# Importar modelo
+from Login.models import Example2
+# Importar Serializer
+from Login.serializer import Example2Serializers
+
+class CustonAuthToken(ObtainAuthToken):
+    
+    def post(self, request, * args, **kwars):
+        serializer = self.serializer_class (data = request.data, 
+                                            context = {
+                                                    'request': request, 
+                                                    }
+                                            )
+        serializer.is_valid(raise_exception = True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })
+
+class ExampleList2(APIView):
+    # Método GET para solciitar info
+    def get(self, request, format=None):
+        print("Método get")
+        queryset = Example2.objects.filter(delete = False)
+        #many = True Si aplica si retorno  multiples objetos
+        serializer = Example2Serializers(queryset, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        print("Método post")
+        serializer = Example2Serializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            datas = serializer.data
+            return Response(datas)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
