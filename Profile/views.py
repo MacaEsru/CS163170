5d972a2e0f65eafab355b78fb294f09a30927fd6
MacaEsru <163170@ids.upchiapas.edu.mx>
2020-03-17 14:46:34 -0600

    add isAuthenticated in views.py

--- Profile/views.py
+++ Profile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 
 from rest_framework.views import APIView
+from rest_framework.schemas import AutoSchema
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
@@ -20,8 +21,22 @@ from Profile.serializer import MaritalStatusSerializers
 from Profile.serializer import GenderSerializers
 from Profile.serializer import OccupationSerializers
 
+import coreapi
+from rest_framework.permissions import IsAuthenticated
+
+class ProfileListViewSchema(AutoSchema):
+    def get_manual_fields(self,path,method):
+        extra_fields = []
+        if method.lower() in ('post','get'):
+            extra_fields = [
+                coreapi.Field('nombre')
+            ]
+        manual_fields =super().get_manual_fields(path,method)
+        return manual_fields + extra_fields
 
 class ProfileList(APIView):
+    permission_classes = [IsAuthenticated,]
+    schema = ProfileListViewSchema()
     # Método GET para solciitar info
     def get(self, request, format=None):
         print("Método get")
@@ -40,6 +55,8 @@ class ProfileList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CityList(APIView):
+    permission_classes = [IsAuthenticated,]
+    schema = ProfileListViewSchema()
     # Método GET para solciitar info
     def get(self, request, format=None):
         print("Método get")
@@ -58,6 +75,8 @@ class CityList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class StateList(APIView):
+    permission_classes = [IsAuthenticated,]
+    schema = ProfileListViewSchema()
     # Método GET para solciitar info
     def get(self, request, format=None):
         print("Método get")
@@ -76,6 +95,8 @@ class StateList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class MaritalStatusList(APIView):
+    permission_classes = [IsAuthenticated,]
+    schema = ProfileListViewSchema()
     # Método GET para solciitar info
     def get(self, request, format=None):
         print("Método get")
@@ -94,6 +115,8 @@ class MaritalStatusList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class GenderList(APIView):
+    permission_classes = [IsAuthenticated,]
+    schema = ProfileListViewSchema()
     # Método GET para solciitar info
     def get(self, request, format=None):
         print("Método get")
@@ -112,6 +135,8 @@ class GenderList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class OccupationList(APIView):
+    permission_classes = [IsAuthenticated,]
+    schema = ProfileListViewSchema()
     # Método GET para solciitar info
     def get(self, request, format=None):
         print("Método get")
